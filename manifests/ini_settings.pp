ac3a69fe4d72c41c5f24a5ed913f6893e44482ae
Jostein Martinsen <josm@redpill-linpro.com>
2017-09-14 15:35:27 +0200

    mer fixar

--- manifests/ini_settings.pp
+++ manifests/ini_settings.pp
@@ -7,10 +7,10 @@ class health::ini_settings (
   validate_hash($settings)
   $defaults = { 'path' => "${path}/${inifilename}" }
 
- 
   file { $path: 
     ensure => directory,
-    mode   => '0750';
+    mode   => '0750',
+    before => Define["Create_ini_settings"];
   }
 
   create_ini_settings($settings, $defaults)
--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,9 @@ class health (
   $settings
 ) {
 
+  # Deploy .ini file with settings for the healthscript
+  include health::ini_settings
+
   validate_hash($settings)
   create_resources('health::healthscript', $settings )
 
