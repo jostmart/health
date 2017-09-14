8dedd895b310ec503131c8e3aac9a8f79c9f623f
Jostein Martinsen <josm@redpill-linpro.com>
2017-09-14 15:43:11 +0200

    boring

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,13 @@
 # 
 #
 class health (
-  $settings
+  $settings,
+  $ini_configdir = '/srv/health'
 ) {
+
+  file { $ini_configdir:
+    ensure => present;
+  }
 
   # Deploy .ini file with settings for the healthscript
   include health::ini_settings
